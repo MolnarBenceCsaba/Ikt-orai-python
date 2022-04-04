8b288ea2852ce992250c8c82e574e731719e4444
MolnarBenceCsaba <bencecsababence5@gmail.hu>
2022-04-04 13:16:18 +0200

    utolso commit halalom elott

--- hodo.py
+++ hodo.py
@@ -13,14 +13,14 @@ gomb3.pack(side=RIGHT)
 
 mezo1 = Entry(foablak)
 
-cimke2 = Label(foablak, text="Dolgok")
+cimke2 = Label(foablak, text="Mennyiség ")
 
 cimke2.pack(side=RIGHT)
 mezo1.pack(side=RIGHT)
 
 mezo2 = Entry(foablak)
 
-cimke3 = Label(foablak, text="Dolgok")
+cimke3 = Label(foablak, text="hány cm")
 
 cimke3.pack(side=RIGHT)
 mezo2.pack
@@ -34,13 +34,10 @@ cimke1 = Label(foablak, text="Dolgok")
 cimke1.pack(side=RIGHT)
 mezo4 = Entry(foablak)
 
-
 mezo4.pack(side=RIGHT)
 
+
 mezo5 = Entry(foablak)
-
-
-
 
 mezo5.pack(side=RIGHT)
 
